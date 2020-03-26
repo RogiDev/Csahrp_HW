edbdaff122effa8a48955966b96cb9851da70699
Igor Pogozhy <igordeveloper92@gmail.com>
2020-03-26 15:33:30 +0200

    C# home work

--- Hw1/Program.cs
+++ Hw1/Program.cs
@@ -7,19 +7,23 @@ namespace Hw1
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Please Enter Array Length");
+            //takes the array length from user input and convert to int --- user input always string
             int arrayLen = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please Enter a number");
+            // takes a mul number from user and convert it to int from string
             int mulNum = Convert.ToInt32(Console.ReadLine());
+            //new array initilize
             int[] myArray = new int[arrayLen];
-
+            // for loop to print the numbers
             for (int i = 0; i < myArray.Length; i++)
             {
                 myArray[i] = i * mulNum;
                 Console.Write($"{myArray[i]} ");
                 
             }
-
+            // wait from user to press some key
             Console.ReadKey();
         }
     }
