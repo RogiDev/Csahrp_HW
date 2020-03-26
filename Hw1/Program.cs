a95b27ea45626ee4b605dbf546ae7abe2ae66ab2
Igor Pogozhy <igordeveloper92@gmail.com>
2020-03-26 15:28:45 +0200

    C# home work

---
+++ Hw1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Array Length");
+            int arrayLen = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter a number");
+            int mulNum = Convert.ToInt32(Console.ReadLine());
+            int[] myArray = new int[arrayLen];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = i * mulNum;
+                Console.Write($"{myArray[i]} ");
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
