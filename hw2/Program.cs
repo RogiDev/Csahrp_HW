7efebcd2e7ef2ef43a18e648de63ff91c06db2e3
RogiDev <31067797+RogiDev@users.noreply.github.com>
2020-04-07 20:04:31 +0300

    hw2

---
+++ hw2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace hw3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] strarr =
+            {
+                new string[] {"main", "up", "sub"},
+                new string[] {"down", "center", "up"},
+                new string[]{"sub","down","main"}
+            };
+          
+            for (int i = 0; i < strarr.Length;i++)
+            {
+                for(int j = 0; j<strarr.Length; j++)
+                {
+                    Console.Write(strarr[i][j] + " ");
+                }
+                Console.WriteLine("\n");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
